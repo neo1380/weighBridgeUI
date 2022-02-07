f8306ee7478d2f73e1ef0ff68e46dc5fb030df66
dineshthoota <dineshbabu.thoota@hotmail.com>
2022-02-07 22:55:42 +0530

    Initial folder structure created

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        );
+    }
+}
+ 
+export default Button;
