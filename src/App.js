853426752c624e2701e87279336cf52be89285ac
dineshthoota <dineshbabu.thoota@hotmail.com>
2022-02-09 23:12:07 +0530

    Dependencies added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Button from "components/Button";
+// import Button from "components/Button";
+import { Button } from 'antd';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
          Weigh Bridge Application
-         <Button/>
+         <Button type="primary">Primary Button</Button>
       </header>
     </div>
   );
