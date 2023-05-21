59a07efa620100f87ca53cbfb84f124e42124eea
dineshthoota <dineshbabu.thoota@hotmail.com>
2023-05-21 11:19:09 +0530

    prod url coded

--- src/constants/api.constants.js
+++ src/constants/api.constants.js
@@ -2,9 +2,10 @@
 // export const BASE_URL = "http://ec2-13-233-224-164.ap-south-1.compute.amazonaws.com/";
 // export const BASE_URL = "https://localhost:8443/";
 // export const BASE_URL =  "https://ec2-13-232-33-95.ap-south-1.compute.amazonaws.com:8443/";
+// export const BASE_URL = "https://ec2-13-232-33-95.ap-south-1.compute.amazonaws.com:8389/";
+//129 IP is production
 export const BASE_URL =
-  "https://ec2-13-232-33-95.ap-south-1.compute.amazonaws.com:8389/";
-
+  "https://ec2-52-66-201-129.ap-south-1.compute.amazonaws.com:8389/";
 export const AUTH_URL = "https://aldakheel-api-v1.vercel.app/api/v1/";
 
 export const API_ENDPOINTS = {
